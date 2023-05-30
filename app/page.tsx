1ff56b01f37c819c98c2944b41b6b755fb956b87
wortie <arcwo@local>
2023-05-30 14:42:19 +0100

    chore(page) disclaimer & content

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,24 @@
 "use client";
 
+import { WarningCallout } from "nhsuk-react-components";
+
 export default function Page() {
   return (
     <>
       <h1>
         NHS
-        <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-700 via-blue-500 to-purple-700'>
+        <span className='ml-4 text-transparent bg-clip-text bg-gradient-to-r from-blue-700 via-blue-500 to-purple-700'>
           Interactive
         </span>
       </h1>
+      <WarningCallout>
+        <WarningCallout.Label>Disclaimer</WarningCallout.Label>
+        <p>
+          This is a mock service made to look like an NHS website using their
+          frontend toolkit and react components. Information given may also
+          differ from truth. Do not use this for real medical advice or as a source of truth.
+        </p>
+      </WarningCallout>
     </>
   );
 }
