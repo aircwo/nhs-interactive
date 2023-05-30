131bdc78924718c28106116543041a0960f11f89
wortie <arcwo@local>
2023-05-30 14:18:54 +0100

    chore(page) initial index page

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+export default function Page() {
+  return (
+    <>
+      <h1>
+        NHS
+        <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-700 via-blue-500 to-purple-700'>
+          Interactive
+        </span>
+      </h1>
+    </>
+  );
+}
