98449d5647f3db8c5342bfa8cbe25b90c8a79966
wortie <mc8atech@gmail.com>
2023-06-08 15:52:53 +0100

    chore: refactored search into functions (#6)
    * chore: refactored search into functions
    
    * chore(tests) initial hook & function
    
    * chore: fix missing import

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,12 +1,10 @@
 "use client";
 
-import { ChangeEvent, FC, KeyboardEvent, useEffect, useRef, useState } from "react";
+import { ChangeEvent, FC, KeyboardEvent, useRef, useState } from "react";
 import { ActionLink, Button, Input, InsetText } from "nhsuk-react-components";
-import { Source } from "@/types";
-import endent from "endent";
-import { UNRELATED_ANSWER } from "../lib/utils/constants";
 import { SearchProps } from "../lib/utils/interfaces";
 import { useApiKey } from "../lib/utils/hooks";
+import { fetchSources, handleStream } from "../lib/utils/functions";
 
 export const Search: FC<SearchProps> = ({
   onSearch,
@@ -22,6 +20,12 @@ export const Search: FC<SearchProps> = ({
   
   useApiKey(setApiKey);
 
+  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
   const handleSearch = async () => {
     if (!query) {
       setError(true);
@@ -29,84 +33,11 @@ export const Search: FC<SearchProps> = ({
     }
 
     setLoading(true);
-    const sources = await fetchSources();
-    await handleStream(sources);
-  };
-
-  const fetchSources = async () => {
-    const response = await fetch("/api/sources", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ query }),
-    });
-
-    if (!response.ok) {
-      setLoading(false);
-      throw new Error(response.statusText);
-    }
-
-    const { sources }: { sources: Source[] } = await response.json();
-    return sources;
-  };
-
-  const handleStream = async (sources: Source[]) => {
-    if (sources.length <= 0) {
-      setLoading(false);
-      onAnswerUpdate(UNRELATED_ANSWER);
-      onSearch({ query, sourceLinks: []})
-      return;
-    }
     try {
-      const prompt = endent`Provide a 1 to 3 sentence answer (with the last sentence ending with a full stop) to the query based on the following sources. Be original, concise, accurate, and helpful. Cite sources as [1] or [2] or [3] after each sentence (not just the very end) to back up your answer (Ex: Correct: [1], Correct: [2][3], Incorrect: [1, 2]).
-      ${sources
-        .map((source, idx) => `Source [${idx + 1}]:\n${source.text}`)
-        .join("\n\n")}
-      `;
-
-      const response = await fetch("/api/answer", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ prompt, apiKey }),
-      });
-
-      if (!response.ok) {
-        setLoading(false);
-        throw new Error(response.statusText);
-      }
-
+      const sources = await fetchSources(query);
+      await handleStream(query, sources, apiKey, onAnswerUpdate, onSearch, onDone, setLoading);
+    } catch (error) {
       setLoading(false);
-      onSearch({ query, sourceLinks: sources.map((source) => source.url) });
-
-      const data = response.body;
-
-      if (!data) {
-        return;
-      }
-
-      const reader = data.getReader();
-      const decoder = new TextDecoder();
-      let done = false;
-
-      while (!done) {
-        const { value, done: doneReading } = await reader.read();
-        done = doneReading;
-        const chunkValue = decoder.decode(value);
-        onAnswerUpdate(chunkValue);
-      }
-
-      onDone(true);
-    } catch (err) {
-      onAnswerUpdate("Error");
-    }
-  };
-
-  const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === "Enter") {
-      handleSearch();
     }
   };
 
