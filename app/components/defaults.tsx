f1a763ada4fd555578a9fc58ff618f40944fd3b0
wortie <mc8atech@gmail.com>
2023-06-13 22:50:01 +0100

    chore(feature) nhs-blue (#11)
    * chore(tailwind) nhs-blue
    
    * chore: nhs-blue & formatting
    
    * chore(tests) nhs-blue

--- app/components/defaults.tsx
+++ app/components/defaults.tsx
@@ -2,6 +2,17 @@
 import { IconBrandGithub } from "@tabler/icons-react";
 import { OpenAIModel } from "../lib/utils/constants";
 
+/**
+ * `export const GitHubIcon = () => {` is defining a React functional component that returns an anchor tag with the GitHub
+ * icon from the `@tabler/icons-react` library. The anchor tag has a link to the GitHub profile of the user `worti3`. This
+ * component can be imported and used in other parts of the codebase.
+ * 
+ * @function
+ * @name GitHubIcon
+ * @kind variable
+ * @returns {JSX.Element}
+ * @exports
+ */
 export const GitHubIcon = () => {
   return (
     <a
@@ -15,6 +26,17 @@ export const GitHubIcon = () => {
   );
 };
 
+/**
+ * `export const ModelInfo` is defining a React functional component that returns a paragraph element displaying
+ * information about a specific OpenAI model. The model information is accessed through the `OpenAIModel` constant defined
+ * in the `constants.ts` file. This component can be imported and used in other parts of the codebase.
+ * 
+ * @function
+ * @name ModelInfo
+ * @kind variable
+ * @returns {JSX.Element}
+ * @exports
+ */
 export const ModelInfo = () => {
   return (
     <p className="nhsuk-body-s nhsuk-u-secondary-text-color nhsuk-u-margin-top-7 nhsuk-u-margin-bottom-0">
@@ -23,6 +45,19 @@ export const ModelInfo = () => {
   );
 }
 
+/**
+ * This code exports a function named `makeSourcesLinks` that takes in two parameters: `answer` and `sourceLinks`. The
+ * function splits the `answer` string using a regular expression and maps over the resulting array. If a part of the
+ * string matches the regular expression `/\[[0-9]+\]/`, it extracts the number within the square brackets, uses it to
+ * retrieve a link from the `sourceLinks` array, and returns an anchor tag with the link and the original part of the
+ * string. If the part of the string does not match the regular expression, it returns the original part of the string. The
+ * function then returns an array of elements that can be rendered in the UI.
+ * 
+ * @function
+ * @name makeSourcesLinks
+ * @kind variable
+ * @exports
+ */
 export const makeSourcesLinks = (answer: string, sourceLinks: string[]) => {
   const elements = answer.split(/(\[[0-9]+\])/).map((part, index) => {
     if (/\[[0-9]+\]/.test(part)) {
@@ -31,7 +66,7 @@ export const makeSourcesLinks = (answer: string, sourceLinks: string[]) => {
       return (
         <a
           key={index}
-          className="hover:cursor-pointer text-blue-500"
+          className="hover:cursor-pointer text-nhs-blue text-base"
           href={link}
           target="_blank"
           rel="noopener noreferrer"
