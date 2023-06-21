a389b28eb2789e85dd9dc0cf4559e178b0e07bbd
wortie <mc8atech@gmail.com>
2023-06-21 11:18:37 +0100

    chore(tests) minor updates

--- test/utils/hooks.test.ts
+++ test/utils/hooks.test.ts
@@ -28,4 +28,31 @@ describe('useApiLog', () => {
       body: JSON.stringify(logData),
     });
   });
+
+  test('does not send POST request when done is false', async () => {
+    const logData: LogData = {
+      searchQuery: { query: 'test query', sourceLinks: [] },
+      answer: 'test answer',
+    };
+
+    const fetchMock = jest.fn().mockResolvedValueOnce({ ok: true });
+    global.fetch = fetchMock;
+
+    const { rerender } = renderHook(
+      ({ logData, done }) => useApiLog(logData, done),
+      { initialProps: { logData, done: false } }
+    );
+
+    expect(fetchMock).not.toHaveBeenCalled();
+
+    rerender({ logData, done: false });
+
+    expect(fetchMock).not.toHaveBeenCalledWith('/api/log', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(logData),
+    });
+  });
 });
