fc777300d0828ef480fb51b7e543e39335f2ca74
wortie <mc8atech@gmail.com>
2023-07-04 23:06:10 +0100

    chore(mixed) nhs search integration (#13)
    * chore(tests) playwright added
    
    * chore(api) search URL change to UK
    
    * chore(api) append nhs search url
    
    * chore(api) alter google fetch
    
    * chore(api) nhs search on empty google
    
    * chore(api) expand whitelist
    
    * chore(layout) updated landing image
    
    * chore(layout) remove static image

--- app/api/sources/route.ts
+++ app/api/sources/route.ts
@@ -5,8 +5,8 @@ import { cleanSourceText } from "@/app/lib/utils/functions";
 import { load } from "cheerio";
 import { NextResponse } from "next/server";
 
-const SEARCH_URL = "https://www.google.com/search?q=";
-const URL_WHITELIST = ["nhs", "nhsbsa", "gov.uk", "nhs.uk"];
+const SEARCH_URL = "https://www.google.co.uk/search?q=";
+const URL_WHITELIST = ["hscni.net", "nhs24.scot", "nhs.wales", "nhsbsa", "gov.uk", "nhs.uk"];
 
 export async function POST(req: Request): Promise<NextResponse<SourceData>> {
   try {
@@ -57,6 +57,8 @@ export async function POST(req: Request): Promise<NextResponse<SourceData>> {
 
         if (parsed) {
           let sourceText = cleanSourceText(parsed.textContent);
+          console.log("link: " + link);
+          
           return { url: link, text: sourceText };
         }
       })
@@ -67,6 +69,12 @@ export async function POST(req: Request): Promise<NextResponse<SourceData>> {
     for (const source of filteredSources) {
       source.text = source.text.slice(0, 1000);
     }
+
+    if (filteredSources.length <= 0) {
+      console.log("No sources found, searching NHS");
+      const nhsSearchResults = await fetchNhsSearchResults(query);
+      filteredSources.push(...nhsSearchResults);
+    }
     const responseHeaders = {
       "Content-Type": "application/json",
       "Cache-Control": "s-maxage=300, stale-while-revalidate",
@@ -76,4 +84,36 @@ export async function POST(req: Request): Promise<NextResponse<SourceData>> {
     console.log(error);
     return NextResponse.json({ sources: [] }, { status: 500 });
   }
+}
+
+/**
+ * The `fetchNhsSearchResults` function is an asynchronous function that takes a `query` parameter of type string. It
+ * fetches search results from the NHS website based on the provided query.
+ * 
+ * @async
+ * @function
+ * @name fetchNhsSearchResults
+ * @kind function
+ * @param {string} query
+ * @returns {Promise<Source[]>}
+ */
+async function fetchNhsSearchResults(query: string) {
+  const nhsSearchUrl = `https://www.nhs.uk/search/results?q=${query}`;
+  const response = await fetch(nhsSearchUrl);
+  const html = await response.text();
+  const dom = new JSDOM(html);
+  const doc = dom.window.document;
+  const results = doc.querySelectorAll('ul.nhsuk-list li');
+
+  // 3 is the default number of li elements on the page without results.
+  if (results.length <= 3) return [];
+  const sources: Source[] = [];
+  let sourceText = '';
+  results.forEach((result) => {
+    const snippetElement = result.querySelector('p.nhsuk-body-s');
+    const snippet = snippetElement?.textContent ? snippetElement.textContent.trim() : '';
+    sourceText += snippet;
+  });
+  sources.push({ url: nhsSearchUrl, text: sourceText });
+  return sources;
 }
