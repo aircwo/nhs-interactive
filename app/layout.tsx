4a4dc35667d1857f14e629f630c16449b1f52d85
wortie <mc8atech@gmail.com>
2023-07-04 15:15:34 +0100

    chore(layout) more meta tags for link sharing

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,23 @@ import { Metadata } from 'next';
 import { Header } from './components/nhs/Header';
 import { Footer } from './components/nhs/Footer';
 import { wrapper } from './components/nhs/wrapper';
+import { SERVICE_DESC, SERVICE_NAME, SERVICE_URL } from './lib/utils/constants';
 
 export const metadata: Metadata = {
-  title: 'NHS - Interactive',
-  description: 'AI-powered search.',
+  title: SERVICE_NAME,
+  description: SERVICE_DESC,
+  openGraph: {
+    title: SERVICE_NAME,
+    description: SERVICE_DESC,
+    type: 'website',
+    locale: 'en_GB',
+    url: SERVICE_URL,
+  },
+  twitter: {
+    title: SERVICE_NAME,
+    description: SERVICE_DESC,
+    card: 'summary',
+  },
 };
 
 export default function RootLayout({
