da8f3bc6555f4d12eed0fa2fa4cde12949f9bfc5
wortie <aircwo@gmail.com>
2024-05-28 11:05:07 +0100

    chore: analytics

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -3,6 +3,7 @@ import './styles/tailwind.scss';
 import { NextIntlClientProvider } from 'next-intl';
 import { Metadata } from 'next';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/react"
 import { Header, Footer, Wrapper } from './components/nhs';
 import { SERVICE_DESC, SERVICE_NAME, SERVICE_URL, checkHealthAPIStatus, getInternationalisation } from '../utils';
 
@@ -41,6 +42,7 @@ export default async function RootLayout({
         <Footer />
       </NextIntlClientProvider>
       <SpeedInsights />
+      <Analytics />
       </body>
     </html>
   );
